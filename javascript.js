96d6fe88344ca5ceafb7d1d72e2e30af45d66308
Favio <145404254+favioAC@users.noreply.github.com>
2024-07-23 22:33:08 -0500

    Create computer and human choice variables.
    Added function to calculate computer choice.
    Added function to get valid user choice.

--- javascript.js
+++ javascript.js
@@ -3,3 +3,31 @@
 // Determine round winner
 // Initialize counter to keep score
 // First to 5 wins
+
+function getComputerChoice () {
+    const number = Math.floor(Math.random() * 100);
+    return number > 66 ? 'rock'
+        : number > 33 ? 'paper'
+        : 'scissors';
+}
+
+function getHumanChoice () {
+    let humanInput = prompt('Make a selection: Rock, Paper, or Scissors.', '');
+    if (humanInput.toLowerCase() === 'rock') {
+        return 'rock';
+    } else if (humanInput.toLowerCase() === 'paper') {
+        return 'paper';
+    } else if (humanInput.toLowerCase() === 'scissors') {
+        return 'scissors';
+    } else {
+        alert(`You entered \"${humanInput}\". Make a valid selection from Rock, Paper, or Scissors.`);
+        return getHumanChoice();
+        
+    }
+}
+
+const computerChoice = getComputerChoice();
+let humanChoice = getHumanChoice();
+
+console.log(`The computer chose ${computerChoice}.`);
+console.log(`You chose ${humanChoice}.`);
